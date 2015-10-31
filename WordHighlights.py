8d6e8d0897d4ddacfcd68d10326f602111b71e87
German M. Bravo <german.mb@deipi.com>
2015-10-31 12:27:57 -0600

    Selections can be highlighted now with any color (modifies scheme file)

--- WordHighlights.py
+++ WordHighlights.py
@@ -1,15 +1,209 @@
+import os
+import re
+
 import sublime
 import sublime_plugin
 
-DEFAULT_COLOR_SCOPE_NAME = 'comment'
+from .colors import names_to_hex, xterm_to_hex
+all_names_to_hex = dict(names_to_hex, **xterm_to_hex)
+
 
 ALL_SETTINGS = [
     'word_highlights',
+    'word_highlights_default_color',
+    'word_highlights_live_color',
     'word_highlights_draw_outlined',
     'word_highlights_when_selection_is_empty',
 ]
 
-used_color_scope_names = []
+
+def log(s):
+    # print("[WordHighlights]", s)
+    pass
+
+
+class HtmlGen:
+    name = "Color"
+    prefix = "col_"
+    backup_ext = ".chback"
+
+    colors = {}
+    color_scheme = None
+    need_upd = False
+    need_restore = False
+    need_backup = False
+    gen_string = """
+        <dict>
+            <key>name</key>
+            <string>%s</string>
+            <key>scope</key>
+            <string>%s</string>
+            <key>settings</key>
+            <dict>
+                <key>background</key>
+                <string>%s</string>
+                <key>caret</key>
+                <string>%s</string>
+                <key>foreground</key>
+                <string>%s</string>
+            </dict>
+        </dict>
+"""
+
+    def normalize(self, col):
+        if col:
+            col = all_names_to_hex.get(col.lower(), col.upper())
+            if col.startswith('0X'):
+                col = '#' + col[2:]
+            try:
+                if col[0] != '#':
+                    raise ValueError
+                if len(col) == 4:
+                    col = '#' + col[1] * 2 + col[2] * 2 + col[3] * 2 + 'FF'
+                elif len(col) == 5:
+                    col = '#' + col[1] * 2 + col[2] * 2 + col[3] * 2 + col[4] * 2
+                elif len(col) == 7:
+                    col += 'FF'
+                return '#%02X%02X%02X%02X' % (int(col[1:3], 16), int(col[3:5], 16), int(col[5:7], 16), int(col[7:9], 16))
+            except Exception:
+                print("Invalid color: %r" % col)
+
+    def write_file(self, pp, fl, s):
+        rf = pp + fl
+        dn = os.path.dirname(rf)
+        if not os.path.exists(dn):
+            os.makedirs(dn)
+        f = open(rf, 'w')
+        f.write(s)
+        f.close()
+
+    def read_file(self, pp, fl):
+        rf = pp + fl
+        if os.path.exists(rf):
+            f = open(rf, 'r')
+            res = f.read()
+            f.close()
+        else:
+            rf = 'Packages' + fl
+            res = sublime.load_resource(rf)
+        return res
+
+    def get_y(self, col):
+        return (0.3 * int(col[1:3], 16) + 0.59 * int(col[3:5], 16) + 0.11 * int(col[5:7], 16)) * (int(col[7:9], 16) / 255.0)
+
+    def get_fg_col(self, col):
+        if self.get_y(col) > 255.0 / 2:
+            return '#000000FF'
+        return '#FFFFFFFF'
+
+    def region_name(self, s):
+        return self.prefix + s[1:]
+
+    def add_color(self, col):
+        col = self.normalize(col)
+        if not col:
+            return
+        if col not in self.colors:
+            self.colors[col] = self.region_name(col)
+            self.need_upd = True
+        return self.colors[col]
+
+    def need_update(self):
+        return self.need_upd
+
+    def color_scheme_path(self, view):
+        packages_path = sublime.packages_path()
+        cs = self.color_scheme
+        if cs is None:
+            self.color_scheme = view.settings().get('color_scheme')
+            cs = self.color_scheme
+        # do not support empty color scheme
+        if not cs:
+            log("Empty scheme")
+            return
+        # extract name
+        cs = cs[cs.find('/'):]
+        return packages_path, cs
+
+    def get_color_scheme(self, packages_path, cs):
+        cont = self.read_file(packages_path, cs)
+        if os.path.exists(packages_path + cs + self.backup_ext):
+            log("Already backuped")
+        else:
+            self.write_file(packages_path, cs + self.backup_ext, cont)  # backup
+            log("Backup done")
+        return cont
+
+    def update(self, view):
+        if not self.need_upd:
+            return
+        self.need_upd = False
+
+        color_scheme_path = self.color_scheme_path(view)
+        if not color_scheme_path:
+            return
+        packages_path, cs = color_scheme_path
+        cont = self.get_color_scheme(packages_path, cs)
+
+        current_colors = set("#%s" % c for c in re.findall(r'<string>%s(.*?)</string>' % self.prefix, cont, re.DOTALL))
+
+        string = ""
+        for col, name in self.colors.items():
+            if col not in current_colors:
+                fg_col = self.get_fg_col(col)
+                string += self.gen_string % (self.name, name, col, fg_col, fg_col)
+
+        if string:
+            # edit cont
+            n = cont.find("<array>") + len("<array>")
+            try:
+                cont = cont[:n] + string + cont[n:]
+            except UnicodeDecodeError:
+                cont = cont[:n] + string.encode("utf-8") + cont[n:]
+
+            self.write_file(packages_path, cs, cont)
+            self.need_restore = True
+
+    def restore_color_scheme(self):
+        if not self.need_restore:
+            return
+        self.need_restore = False
+        cs = self.color_scheme
+        # do not support empty color scheme
+        if not cs:
+            log("Empty scheme, can't restore")
+            return
+        # extract name
+        cs = cs[cs.find('/'):]
+        packages_path = sublime.packages_path()
+        if os.path.exists(packages_path + cs + self.backup_ext):
+            log("Starting restore scheme: " + cs)
+            # TODO: move to other thread
+            self.write_file(packages_path, cs, self.read_file(packages_path, cs + self.backup_ext))
+            self.colors = {}
+            log("Restore done.")
+        else:
+            log("No backup :(")
+
+    def set_color_scheme(self, view):
+        cs = view.settings().get('color_scheme')
+        if cs != self.color_scheme:
+            color_scheme_path = self.color_scheme_path(view)
+            if color_scheme_path:
+                packages_path, cs = color_scheme_path
+                cont = self.get_color_scheme(packages_path, cs)
+                self.colors = dict(("#%s" % c, "%s%s" % (self.prefix, c)) for c in re.findall(r'<string>%s(.*?)</string>' % self.prefix, cont, re.DOTALL))
+            self.color_scheme = view.settings().get('color_scheme')
+            self.need_backup = True
+
+    def change_color_scheme(self, view):
+        cs = view.settings().get('color_scheme')
+        if cs != self.color_scheme:
+            self.restore_color_scheme()
+            self.set_color_scheme(view)
+            self.update(view)
+
+htmlGen = HtmlGen()
 
 
 def settings_changed():
@@ -49,10 +243,16 @@ def regex_escape(string):
     return outstring
 
 
-def highlight(view, color_scope_name=DEFAULT_COLOR_SCOPE_NAME, when_selection_is_empty=False):
+def highlight(view, color=None, when_selection_is_empty=False):
     settings = view.settings()
     draw_outlined = sublime.DRAW_OUTLINED if settings.get('word_highlights_draw_outlined') else 0
     word_separators = settings.get('word_separators')
+
+    if color is None:
+        color = settings.get('word_highlights_default_color')
+    color_scope_name = htmlGen.add_color(color) or 'comment'
+    if htmlGen.need_update():
+        htmlGen.update(view)
 
     view_sel = view.sel()
     if len(view_sel) > 1:
@@ -78,21 +278,25 @@ def highlight(view, color_scope_name=DEFAULT_COLOR_SCOPE_NAME, when_selection_is
                     else:
                         regex = regex_escape(string)
                     regions += view.find_all(regex)
-    if color_scope_name not in used_color_scope_names:
-        used_color_scope_names.append(color_scope_name)
-    view.add_regions('WordHighlights_%s' % color_scope_name, regions, color_scope_name, '', draw_outlined | sublime.PERSISTENT)
+    view.add_regions('wh_' + color_scope_name, regions, color_scope_name, '', draw_outlined | sublime.PERSISTENT)
 
 
 def reset(view):
-    while used_color_scope_names:
-        view.erase_regions('WordHighlights_%s' % used_color_scope_names.pop())
+    for color_scope_name in htmlGen.colors.values():
+        view.erase_regions('wh_' + color_scope_name)
 
 
 class WordHighlightsListener(sublime_plugin.EventListener):
+    def on_new(self, view):
+        htmlGen.set_color_scheme(view)
+        view.settings().add_on_change('color_scheme', lambda self=self, view=view: htmlGen.change_color_scheme(view))
+
     def on_selection_modified(self, view):
         settings = view.settings()
         if word_highlights_enabled(view, True):
-            highlight(view, DEFAULT_COLOR_SCOPE_NAME, settings.get('word_highlights_when_selection_is_empty', False))
+            word_highlights_live_color = settings.get('word_highlights_live_color')
+            word_highlights_when_selection_is_empty = settings.get('word_highlights_when_selection_is_empty', False)
+            highlight(view, word_highlights_live_color, word_highlights_when_selection_is_empty)
 
 
 class WordHighlightsToggleCommand(sublime_plugin.TextCommand):
@@ -114,7 +318,7 @@ class WordHighlightsToggleCommand(sublime_plugin.TextCommand):
         else:
             settings.set('word_highlights', True)
             settings.set('word_highlights_when_selection_is_empty', True)
-            highlight(self.view, DEFAULT_COLOR_SCOPE_NAME, True)
+            highlight(self.view, None, True)
 
 
 class WordHighlightsResetCommand(sublime_plugin.TextCommand):
@@ -123,5 +327,12 @@ class WordHighlightsResetCommand(sublime_plugin.TextCommand):
 
 
 class WordHighlightsCommand(sublime_plugin.TextCommand):
-    def run(self, edit, block=False, color_scope_name=DEFAULT_COLOR_SCOPE_NAME):
-        highlight(self.view, color_scope_name, True)
+    def run(self, edit, block=False, color=None):
+        if color == "":
+            self.view.window().show_input_panel("Color:", "", self.on_done, None, None)
+        else:
+            highlight(self.view, color, True)
+
+    def on_done(self, color):
+        if color:
+            highlight(self.view, color, True)
