9b562f4cf938c79e6dc8ea7c00655e6b92022950
German M. Bravo <german.mb@deipi.com>
2017-10-27 11:43:39 -0500

    Refactoring (settings and colorizer split)

--- WordHighlights.py
+++ WordHighlights.py
@@ -1,281 +1,15 @@
-import os
-import re
+from __future__ import absolute_import
 
 import sublime
 import sublime_plugin
 
-from .colors import names_to_hex, xterm_to_hex
-all_names_to_hex = dict(names_to_hex, **xterm_to_hex)
+from .settings import Settings, SettingTogglerCommandMixin
+from .colorizer import SchemaColorizer
 
-version = "1.2"
+NAME = "WordHighlights"  # SublimeHighlighter
+VERSION = "2.0"
 
-
-ALL_SETTINGS = [
-    'word_highlights',
-    'word_highlights_default_color',
-    'word_highlights_live_color',
-    'word_highlights_draw_outlined',
-    'word_highlights_when_selection_is_empty',
-]
-
-
-def log(s):
-    # print("[WordHighlights]", s)
-    pass
-
-
-class HtmlGen:
-    name = "Color"
-    prefix = "col_"
-    backup_ext = ".chback"
-
-    colors = {}
-    color_scheme = None
-    need_upd = False
-    need_restore = False
-    need_backup = False
-    gen_string = """
-        <dict>
-            <key>name</key>
-            <string>{name}</string>
-            <key>scope</key>
-            <string>{scope}</string>
-            <key>settings</key>
-            <dict>
-                <key>background</key>
-                <string>{background}</string>
-                <key>foreground</key>
-                <string>{foreground}</string>
-            </dict>
-        </dict>
-"""
-
-    def normalize(self, col):
-        if col:
-            col = all_names_to_hex.get(col.lower(), col.upper())
-            if col.startswith('0X'):
-                col = '#' + col[2:]
-            try:
-                if col[0] != '#':
-                    raise ValueError
-                if len(col) == 4:
-                    col = '#' + col[1] * 2 + col[2] * 2 + col[3] * 2 + 'FF'
-                elif len(col) == 5:
-                    col = '#' + col[1] * 2 + col[2] * 2 + col[3] * 2 + col[4] * 2
-                elif len(col) == 7:
-                    col += 'FF'
-                r = int(col[1:3], 16)
-                g = int(col[3:5], 16)
-                b = int(col[5:7], 16)
-                a = int(col[7:9], 16) or 1  # alpha == 0 doesn't apply alpha in Sublime
-                return '#%02X%02X%02X%02X' % (r, g, b, a)
-            except Exception:
-                print("Invalid color: %r" % col)
-
-    def write_file(self, pp, fl, s):
-        rf = pp + fl
-        dn = os.path.dirname(rf)
-        if not os.path.exists(dn):
-            os.makedirs(dn)
-        f = open(rf, 'w')
-        f.write(s)
-        f.close()
-
-    def read_file(self, pp, fl):
-        rf = pp + fl
-        if os.path.exists(rf):
-            f = open(rf, 'r')
-            res = f.read()
-            f.close()
-        else:
-            rf = 'Packages' + fl
-            res = sublime.load_resource(rf)
-        return res
-
-    def get_inv_col(self, bg_col, col):
-        br = int(bg_col[1:3], 16) / 255.0
-        bg = int(bg_col[3:5], 16) / 255.0
-        bb = int(bg_col[5:7], 16) / 255.0
-
-        r = int(col[1:3], 16) / 255.0
-        g = int(col[3:5], 16) / 255.0
-        b = int(col[5:7], 16) / 255.0
-        a = int(col[7:9], 16) / 255.0
-
-        r = br * (1 - a) + r * a
-        g = bg * (1 - a) + g * a
-        b = bb * (1 - a) + b * a
-
-        # L = (max(r, g, b) + min(r, g, b)) / 2
-        # Y709 = 0.2126 * r + 0.7152 * g + 0.0722 * b
-        Y601 = 0.299 * r + 0.587 * g + 0.114 * b
-
-        v = Y601
-
-        if v >= 0.5:
-            v -= 0.5
-        else:
-            v += 0.5
-
-        return '#%sFF' % (('%02X' % (v * 255)) * 3)
-
-    def region_name(self, s):
-        return self.prefix + s[1:]
-
-    def add_color(self, col):
-        col = self.normalize(col)
-        if not col:
-            return
-        if col not in self.colors:
-            self.colors[col] = self.region_name(col)
-            self.need_upd = True
-        return self.colors[col]
-
-    def need_update(self):
-        return self.need_upd
-
-    def color_scheme_path(self, view):
-        packages_path = sublime.packages_path()
-        cs = self.color_scheme
-        if cs is None:
-            self.color_scheme = view.settings().get('color_scheme')
-            cs = self.color_scheme
-        # do not support empty color scheme
-        if not cs:
-            log("Empty scheme")
-            return
-        # extract name
-        cs = cs[cs.find('/'):]
-        return packages_path, cs
-
-    def get_color_scheme(self, packages_path, cs):
-        cont = self.read_file(packages_path, cs)
-        if os.path.exists(packages_path + cs + self.backup_ext):
-            log("Already backuped")
-        else:
-            self.write_file(packages_path, cs + self.backup_ext, cont)  # backup
-            log("Backup done")
-        return cont
-
-    def update(self, view):
-        if not self.need_upd:
-            return
-        self.need_upd = False
-
-        color_scheme_path = self.color_scheme_path(view)
-        if not color_scheme_path:
-            return
-        packages_path, cs = color_scheme_path
-        cont = self.get_color_scheme(packages_path, cs)
-
-        current_colors = set("#%s" % c for c in re.findall(r'<string>%s(.*?)</string>' % self.prefix, cont, re.DOTALL))
-
-        if hasattr(view, 'style'):
-            bg_col = view.style()['background']
-        else:
-            bg_col = '#333333FF'
-
-        string = ""
-        for col, name in self.colors.items():
-            if col not in current_colors:
-                fg_col = self.get_inv_col(bg_col, col)
-                string += self.gen_string.format(
-                    name=self.name,
-                    scope=name,
-                    background=col,
-                    foreground=fg_col,
-                )
-
-        if string:
-            # edit cont
-            n = cont.find("<array>") + len("<array>")
-            try:
-                cont = cont[:n] + string + cont[n:]
-            except UnicodeDecodeError:
-                cont = cont[:n] + string.encode("utf-8") + cont[n:]
-
-            self.write_file(packages_path, cs, cont)
-            self.need_restore = True
-            log("Updated")
-
-    def restore_color_scheme(self):
-        if not self.need_restore:
-            return
-        self.need_restore = False
-        cs = self.color_scheme
-        # do not support empty color scheme
-        if not cs:
-            log("Empty scheme, can't restore")
-            return
-        # extract name
-        cs = cs[cs.find('/'):]
-        packages_path = sublime.packages_path()
-        if os.path.exists(packages_path + cs + self.backup_ext):
-            log("Starting restore scheme: " + cs)
-            # TODO: move to other thread
-            self.write_file(packages_path, cs, self.read_file(packages_path, cs + self.backup_ext))
-            self.colors = {}
-            log("Restore done.")
-        else:
-            log("No backup :(")
-
-    def set_color_scheme(self, view):
-        settings = view.settings()
-        cs = settings.get('color_scheme')
-        if cs != self.color_scheme:
-            color_scheme_path = self.color_scheme_path(view)
-            if color_scheme_path:
-                packages_path, cs = color_scheme_path
-                cont = self.get_color_scheme(packages_path, cs)
-                self.colors = dict(("#%s" % c, "%s%s" % (self.prefix, c)) for c in re.findall(r'<string>%s(.*?)</string>' % self.prefix, cont, re.DOTALL))
-            self.color_scheme = settings.get('color_scheme')
-            self.need_backup = True
-
-    def change_color_scheme(self, view):
-        cs = view.settings().get('color_scheme')
-        if cs and cs != self.color_scheme:
-            log("Color scheme changed %s -> %s" % (self.color_scheme, cs))
-            self.restore_color_scheme()
-            self.set_color_scheme(view)
-            self.update(view)
-
-htmlGen = HtmlGen()
-
-
-def settings_changed():
-    for window in sublime.windows():
-        for view in window.views():
-            reload_settings(view.settings())
-
-
-def reload_settings(settings):
-    '''Restores user settings.'''
-    settings_name = 'WordHighlights'
-    global_settings = sublime.load_settings(settings_name + '.sublime-settings')
-    global_settings.clear_on_change(settings_name)
-    global_settings.add_on_change(settings_name, settings_changed)
-
-    for setting in ALL_SETTINGS:
-        if global_settings.has(setting):
-            settings.set(setting, global_settings.get(setting))
-
-    if not settings.has('word_highlights'):
-        settings.set('word_highlights', True)
-
-    if not settings.has('word_highlights_default_color'):
-        settings.set('word_highlights_default_color', "")
-
-    if not settings.has('word_highlights_live_color'):
-        settings.set('word_highlights_live_color', settings.get('word_highlights_default_color'))
-
-    if not settings.has('word_highlights_when_selection_is_empty'):
-        settings.set('word_highlights_when_selection_is_empty', False)
-
-
-def get_setting(settings, name):
-    if not settings.has(name):
-        reload_settings(settings)
-    return settings.get(name)
+colorizer = SchemaColorizer()
 
 
 def regex_escape(string):
@@ -289,9 +23,10 @@ def regex_escape(string):
 
 
 def highlight(view, color=None, when_selection_is_empty=False, add_selections=False):
-    settings = view.settings()
-    draw_outlined = sublime.DRAW_OUTLINED if get_setting(settings, 'word_highlights_draw_outlined') else 0
-    word_separators = settings.get('word_separators')
+    view_settings = view.settings()
+    word_separators = view_settings.get('word_separators')
+
+    draw_outlined = sublime.DRAW_OUTLINED if settings.get('draw_outlined') else 0
 
     view_sel = view.sel()
     regions = []
@@ -323,9 +58,9 @@ def highlight(view, color=None, when_selection_is_empty=False, add_selections=Fa
     else:
         sublime.status_message("")
 
-    color_scope_name = htmlGen.add_color(color) or 'comment'
-    if htmlGen.need_update():
-        htmlGen.update(view)
+    color_scope_name = colorizer.add_color(color) or 'comment'
+    if colorizer.need_update():
+        colorizer.update(view)
     view.add_regions('wh_' + color_scope_name, regions, color_scope_name, '', draw_outlined | sublime.PERSISTENT)
 
     if add_selections:
@@ -333,50 +68,26 @@ def highlight(view, color=None, when_selection_is_empty=False, add_selections=Fa
 
 
 def reset(view):
-    for color_scope_name in htmlGen.colors.values():
+    for color_scope_name in colorizer.colors.values():
         view.erase_regions('wh_' + color_scope_name)
 
 
 # command to restore color scheme
 class RestoreColorSchemeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        htmlGen.restore_color_scheme()
+        colorizer.restore_color_scheme()
 
 
 class WordHighlightsListener(sublime_plugin.EventListener):
     def on_new(self, view):
-        htmlGen.set_color_scheme(view)
-        view.settings().add_on_change('color_scheme', lambda self=self, view=view: htmlGen.change_color_scheme(view))
+        colorizer.set_color_scheme(view)
+        view.settings().add_on_change('color_scheme', lambda self=self, view=view: colorizer.change_color_scheme(view))
 
     def on_selection_modified(self, view):
-        settings = view.settings()
-        if get_setting(settings, 'word_highlights'):
-            color = get_setting(settings, 'word_highlights_live_color')
-            when_selection_is_empty = get_setting(settings, 'word_highlights_when_selection_is_empty')
+        if settings.get('live'):
+            color = settings.get('live_color')
+            when_selection_is_empty = settings.get('when_selection_is_empty')
             highlight(view, color=color, when_selection_is_empty=when_selection_is_empty)
-
-
-class WordHighlightsToggleCommand(sublime_plugin.TextCommand):
-    _word_highlights = None
-    _word_highlights_when_selection_is_empty = None
-
-    def run(self, edit, block=False):
-        settings = self.view.settings()
-        _word_highlights = get_setting(settings, 'word_highlights')
-        _word_highlights_when_selection_is_empty = get_setting(settings, 'word_highlights_when_selection_is_empty')
-        if self.__class__._word_highlights is None:
-            self.__class__._word_highlights = _word_highlights
-        if self.__class__._word_highlights_when_selection_is_empty is None:
-            self.__class__._word_highlights_when_selection_is_empty = _word_highlights_when_selection_is_empty
-        if _word_highlights_when_selection_is_empty and _word_highlights:
-            settings.set('word_highlights', self.__class__._word_highlights)
-            settings.set('word_highlights_when_selection_is_empty', self.__class__._word_highlights_when_selection_is_empty)
-            reset(self.view)
-        else:
-            settings.set('word_highlights', True)
-            settings.set('word_highlights_when_selection_is_empty', True)
-            color = get_setting(settings, 'word_highlights_default_color')
-            highlight(self.view, color=color, when_selection_is_empty=True)
 
 
 class WordHighlightsResetCommand(sublime_plugin.TextCommand):
@@ -392,10 +103,36 @@ class WordHighlightsCommand(sublime_plugin.TextCommand):
             self.view.window().show_input_panel("Color:", "", self.on_done, None, None)
         else:
             if not color:
-                settings = self.view.settings()
-                color = get_setting(settings, 'word_highlights_default_color')
+                color = settings.get('default_color')
             highlight(self.view, color=color, when_selection_is_empty=True)
 
     def on_done(self, color):
         if color:
             highlight(self.view, color=color, when_selection_is_empty=True)
+
+
+################################################################################
+# Initialize settings and main objects only once
+class WordHighlightsSettings(Settings):
+    pass
+
+
+if 'settings' not in globals():
+    settings = WordHighlightsSettings(NAME)
+
+    class WordHighlightsToggleSettingCommand(SettingTogglerCommandMixin, sublime_plugin.WindowCommand):
+        settings = settings
+
+
+################################################################################
+
+def plugin_loaded():
+    settings.load()
+
+
+# ST3 features a plugin_loaded hook which is called when ST's API is ready.
+#
+# We must therefore call our init callback manually on ST2. It must be the last
+# thing in this plugin (thanks, beloved contributors!).
+if int(sublime.version()) < 3000:
+    plugin_loaded()
