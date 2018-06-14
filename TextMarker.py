e5a57ee27f0dd7b980651b2110992b74887e51c3
German M. Bravo <german.mb@gmail.com>
2018-06-14 10:43:29 -0500

    Version bump to 1.0.3

--- TextMarker.py
+++ TextMarker.py
@@ -8,7 +8,7 @@ from .settings import Settings, SettingTogglerCommandMixin
 from .colorizer import SchemaColorizer
 
 NAME = "TextMarker"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 DEFAULT_COLORS = ['comment']
 
